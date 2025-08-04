892c96846e57517c9c762ead9cc0503de13ad9dc
OpenCoreAI <aryansiblue@gmail.com>
2025-08-04 17:12:29 +0530

    Create App.jsx for S1-mini frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>S1-mini AI Interface</h1>
+      <div className="chat-container">
+        {/* Chat interface will be implemented here */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
